fb32fd16a5071144981d5da01987deb40aa0195b
gbolahan <adekoyegbolahan001@gmail.com>
2023-03-01 16:35:11 +0100

    first commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Gbolahan on 23/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
